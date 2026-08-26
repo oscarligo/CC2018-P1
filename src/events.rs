9a56536a1eec51ba66631336bb17d8909b49733b
Oscar Rompich <oscarligo6@gmail.com>
2026-08-26 01:07:59 -0600

    refactor: player's movement

--- src/events.rs
+++ src/events.rs
@@ -3,15 +3,15 @@ use raylib::prelude::*;
 
 pub fn process_events(window: &RaylibHandle, player: &mut Player) {
     if window.is_key_down(KeyboardKey::KEY_W) {
-        player.move_forward(player.move_speed);
+        player.move_forward();
     }
     if window.is_key_down(KeyboardKey::KEY_S) {
-        player.move_backward(player.move_speed);
+        player.move_backward();
     }
     if window.is_key_down(KeyboardKey::KEY_A) {
-        player.rotate_left(player.rotate_speed);
+        player.rotate_left();
     }
     if window.is_key_down(KeyboardKey::KEY_D) {
-        player.rotate_right(player.rotate_speed);
+        player.rotate_right();
     }
 }   
--- src/player.rs
+++ src/player.rs
@@ -6,12 +6,12 @@ pub struct Player {
     pub position: Vector2,
     pub angle: f32,
     pub move_speed: f32,
-    pub rotate_speed: f32,
+    pub rotation_speed: f32,
 }
 
 impl Player {
-    pub fn new(position: Vector2, angle: f32, move_speed: f32, rotate_speed: f32) -> Self {
-        Player { position, angle, move_speed, rotate_speed }
+    pub fn new(position: Vector2, angle: f32, move_speed: f32, rotation_speed: f32) -> Self {
+        Player { position, angle, move_speed, rotation_speed }
     }
 
     pub fn get_position(&self) -> Vector2 {
@@ -25,22 +25,30 @@ impl Player {
     pub fn set_position(&mut self, position: Vector2) {
         self.position = position;
     }
-    pub fn move_forward(&mut self, distance: f32) {
-        self.position.x += distance * self.angle.cos();
-        self.position.y += distance * self.angle.sin();
+    pub fn move_forward(&mut self) {
+        self.position.x += self.move_speed * self.angle.cos();
+        self.position.y += self.move_speed * self.angle.sin();
     }
 
-    pub fn move_backward(&mut self, distance: f32) {
-        self.position.x -= distance * self.angle.cos();
-        self.position.y -= distance * self.angle.sin();
+    pub fn move_backward(&mut self) {
+        self.position.x -= self.move_speed * self.angle.cos();
+        self.position.y -= self.move_speed * self.angle.sin();
     }
 
-    pub fn rotate_left(&mut self, angle: f32) {
-        self.angle -= angle;
+    pub fn rotate_left(&mut self) {
+        self.angle -= self.rotation_speed;
         if self.angle < 0.0 {
-            self.angle += 2.0 * PI;
+            self.angle += 2.0 * PI; 
         }
     }
+
+    pub fn rotate_right(&mut self) {
+        self.angle += self.rotation_speed;
+        if self.angle > 2.0 * PI {
+            self.angle -= 2.0 * PI; 
+        }
+    }
+
 
     pub fn set_initial_position(&mut self, maze: &Maze) {
         for (j, row) in maze.iter().enumerate() {
@@ -54,10 +62,5 @@ impl Player {
         }
     }
 
-    pub fn rotate_right(&mut self, angle: f32) {
-        self.angle += angle;
-        if self.angle >= 2.0 * PI {
-            self.angle -= 2.0 * PI;
-        }
-    }
+    
 }
